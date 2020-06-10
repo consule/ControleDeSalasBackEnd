d8f8629a6b1c040b0d8183a1fad485acf9dd019e
Cristiano Consule <cristianoconsule@gmail.com>
2020-06-10 18:32:21 -0300

    Conexão Com Banco de Dados SQLServer
    Realizada a Conexão com o Banco de Dados de nome ControleDeSalas do SQLServer

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ControleDeSalasBackEnd.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "CriFreezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
